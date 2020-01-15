b931919ec84eed81b3b7333ee451756545422344
Felipe Lima <fearlimasi@gmail.com>
2020-01-15 01:45:47 -0300

    Ajustado nome do arquivo SearchController

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController');
-const SearchController = require('./controllers/SeacrhController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
